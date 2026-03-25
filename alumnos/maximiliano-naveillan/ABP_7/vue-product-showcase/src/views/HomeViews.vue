e657bd13f1d1b87a78c634c366f54051accccd9e
MaximilianoNaveillan <maximilianonaveillan@gmail.com>
2026-03-25 17:45:29 -0300

    add lección 3 maximiliano-naveillan

--- alumnos/maximiliano-naveillan/ABP_7/vue-product-showcase/src/views/HomeViews.vue
+++ alumnos/maximiliano-naveillan/ABP_7/vue-product-showcase/src/views/HomeViews.vue
@@ -1,74 +1,87 @@
 <!-- src/views/HomeView.vue -->
 <script setup>
-import { ref, computed, onMounted } from "vue";
+import { onMounted, computed } from "vue";
 import ProductList from "../components/products/ProductList.vue";
-import { productosService } from "../services/productosService.js";
 
-// Array con todos los prductos que devuelve la api
-const productos = ref([]);
+// Importar los stores directamente - sin useStore(),
+// Cada Store es una función que se inboca acá
+import { useProductStore } from "../stores/productos";
+import { useFiltrosStore } from "../stores/filtros";
+import { useFavoritosStore } from "../stores/favoritos";
 
-// Array con las categorías disponibles para el <select>
-const categorias = ref([]);
+const productosStore = useProductStore();
+const filtradosStore = useFiltrosStore();
+const favoritosStore = useFavoritosStore();
 
-// categoría actualmente seleccionada. todas = sin filtro activo
-const categoriaSeleccionada = ref("todas");
+const cargando = computed(() => productosStore.cargando);
+const error = computed(() => productosStore.error);
+const categorias = computed(() => productosStore.categorias);
+const productosFiltrados = computed(() => productosStore.filtrados);
 
-// true mientras esperamos la respuesta de la API
-const cargando = ref(false);
+onMounted(() => productosStore.fetchProductos());
 
-// null si no hay error; string con el mensaje de error si algo falló
-const error = ref(null);
-
-// Computed
-// productosFiltrados se recalcula automaticamente cuando cambian
-// productos o categoriaSeleccionada. No hace falta llamar nada manualmente.
-const productosFiltrados = computed(() => {
-  if (categoriaSeleccionada.value === "todas") return productos.value;
-  return productos.value.filter((item) => item.category === categoriaSeleccionada.value);
-});
-
-async function cargarProductos() {
-  // Reiniciamos el estado antes de cada petición para que
-  // el spinner aparezca y cualquier error anterior desaparezaca.
-  cargando.value = true;
-  error.value = null;
-  try {
-    // Promise.all ejecuta ambas peticiones en paralelo
-    // Es mas rápido que hacer solicitudes en serie (una despues de otra)
-    const [prods, cats] = await Promise.all([
-      productosService.getAll(),
-      productosService.getCategorias(),
-    ]);
-    productos.value = prods;
-    categorias.value = cats;
-  } catch (err) {
-    // Si cualquiera de las dos peticiones falla, llegamoa a este bloque 🤔
-    error.value = "No se pudieron caragar los productos. Verificar tu conxión";
-    console.log("Error al cargar productos", err);
-  } finally {
-    // finally se ejecuta siempre, haya error o no
-    // Así garantizamos que el spinner siempre se oculte
-    cargando.value = false;
-  }
+function cambiarCategoria(cat) {
+  filtradosStore.cambiarCategoria(cat);
 }
 
-// filtrar() es llamada por @change del <select>
-// No necessita hacer nada mas que loggear, por que "productosFiltrados"
-// ya reacciona a "categoriaSeleccionada"
-function filtrar() {
-  console.log("filtrando por", categoriaSeleccionada.value);
+function toggleFavorito(producto) {
+  favoritosStore.toggle(producto);
 }
 
 function onAgregar(producto) {
   alert(`✅ "${producto.title}" agregado al carrito`);
 }
 
-onMounted(cargarProductos);
+/* 
+*********** Acceso directo a state y geters ******
+productosStore.cargando ya es rectivo: si cambia en el store la vista es actualizada.
+Nota: si se destructura el store 
+ej:
+prosuctosStore = {
+    lista: [],
+    cargando: false,
+    error: null,
+    categorias: [],
+  }
+forma tradicional:  
+const lista =  productosStore.lista
+const cargando = productosStore.cargando
+const error = productosStore.error
+
+forma destructurada de obj.
+
+const {lista, cargando, error,categoria} = productosStore 
+*/
+
+/* 
+********** Disparar actions ********
+onMounted: cargar productos cuando el componente entra al DOM --> 
+onMounted(()=> proDuctStore.fetchProductos())
+
+function cambiarCategoria(cat) {
+filtradosStore.cambiarCategoria(cat)
+}
+
+function toggleFavorito(producto) {
+ favoritosStore.toggle(producto)
+}
+
+*/
 </script>
+<!-- 
+En el Template, acceder a los store directamente:
+ v-if="productosStor.cargando"
+ v-else-if="productosStor.error"
+ :productos="productosStor.filtrados"...
+-->
 <template>
   <main class="home">
     <div class="filtrar">
-      <select v-model="categoriaSeleccionada" @change="filtrar" data-cy="filtro-categoría">
+      <select
+        :value="filtradosStore.categoriaActual"
+        @change="cambiarCategoria($event.target.value)"
+        data-cy="filtro-categoría"
+      >
         <option value="todas">Todas las categorías</option>
         <option v-for="(cat, index) in categorias" :key="index" :value="cat">
           {{ cat }}
@@ -80,6 +93,11 @@ onMounted(cargarProductos);
       <div class="spinner"></div>
       <p>Cargando productos ...</p>
     </div>
-    <ProductList :productos="productos" @agregar="onAgregar" />
+
+    <div v-else-if="error">
+      <p>error: {{ error }}</p>
+      <button></button>
+    </div>
+    <ProductList :productos="productosFiltrados" @agregar="onAgregar" />
   </main>
 </template>
